86f6159b2dbf090f2da9251e1826a8036d157e1b
dypublic <dypublic@gmail.com>
2016-11-06 23:38:32 +0800

    add upload file head function

--- websocket/ws_gorilla_server.go
+++ websocket/ws_gorilla_server.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// +build ignore
+// //+build ignore
 
 package main
 
@@ -13,11 +13,50 @@ import (
 	"net/http"
 
 	"github.com/gorilla/websocket"
+	"fmt"
 )
 
 var addr = flag.String("addr", "localhost:8080", "http service address")
 
 var upgrader = websocket.Upgrader{} // use default options
+
+type FileHead struct {
+	file_name string `json:"file_name"`
+	file_size int    `json:"file_size"`
+}
+
+func upload(w http.ResponseWriter, r *http.Request) {
+	c, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Print("upgrade:", err)
+		return
+	}
+
+	have_head := false
+
+	defer c.Close()
+	for {
+		head := &FileHead{}
+		if !have_head {
+			err := c.ReadJSON(&head)
+			if err != nil{
+				fmt.Println("head not correct!")
+				break
+			}
+		}
+		mt, message, err := c.ReadMessage()
+		if err != nil {
+			log.Println("read:", err)
+			break
+		}
+		log.Printf("recv: %s", message)
+		err = c.WriteMessage(mt, message)
+		if err != nil {
+			log.Println("write:", err)
+			break
+		}
+	}
+}
 
 func echo(w http.ResponseWriter, r *http.Request) {
 	c, err := upgrader.Upgrade(w, r, nil)
@@ -50,6 +89,7 @@ func main() {
 	log.SetFlags(0)
 	http.HandleFunc("/echo", echo)
 	http.HandleFunc("/", home)
+	http.HandleFunc("/upload", upload)
 	log.Fatal(http.ListenAndServe(*addr, nil))
 }
 
