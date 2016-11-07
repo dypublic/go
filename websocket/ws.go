d9fcd4d809adc5be7721f8e8f741464708960069
dypublic <dypublic@gmail.com>
2016-11-07 11:09:40 +0800

    change check handshake

--- websocket/ws.go
+++ websocket/ws.go
@@ -2,26 +2,36 @@
 package main
 
 import (
-	"golang.org/x/net/websocket"
-	//    "io"
 	"fmt"
+	//	"io"
 	"net/http"
+	"os"
+
+	"github.com/gorilla/handlers"
+	"golang.org/x/net/websocket"
 )
 
 func echoHandler(ws *websocket.Conn) {
 
 	for {
-		receivedtext := make([]byte, 100)
+		//		receivedtext := make([]byte, 100)
 
-		n, err := ws.Read(receivedtext)
+		//		n, err := ws.Read(receivedtext)
 
-		if err != nil {
-			fmt.Printf("Received: %d bytes\n", n)
+		//		if err != nil {
+		//			fmt.Printf("Err Received: %d bytes\n", n)
+		//		}
+
+		//		s := string(receivedtext[:n])
+		//		fmt.Printf("Received: %d bytes: %s\n", n, s)
+		//		io.Copy(ws, ws)
+
+		var in []byte
+		if err := websocket.Message.Receive(ws, &in); err != nil {
+			return
 		}
-
-		s := string(receivedtext[:n])
-		fmt.Printf("Received: %d bytes: %s\n", n, s)
-		//io.Copy(ws, ws)
+		fmt.Printf("Received: %s\n", string(in))
+		websocket.Message.Send(ws, in)
 		//fmt.Printf("Sent: %s\n",s)
 	}
 }
@@ -30,7 +40,7 @@ func main() {
 	s := websocket.Server{Handler: echoHandler}
 	http.Handle("/echo", s)
 	http.Handle("/", http.FileServer(http.Dir(".")))
-	err := http.ListenAndServe(":8080", nil)
+	err := http.ListenAndServe("127.0.0.1:8080", nil)
 	if err != nil {
 		panic("Error: " + err.Error())
 	}
